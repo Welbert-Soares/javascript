c66f214b1fe8844d5d658ed7c683c447e7c8b5b2
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2023-10-30 13:12:56 -0300

    aprendenfo a debugar no vscode

---
+++ aula14/ambiente.js
@@ -0,0 +1,16 @@
+// Programa para testar o laço while
+
+var c = 1;
+while (c <= 5) {
+  // Enquanto c for menor ou igual a 5 faça:
+  console.log(c);
+  c++;
+}
+console.log("FIM");
+console.log("Recomeçando...");
+// Programa para testar o laço for
+var d = 1;
+for (d; d <= 3; d++) {
+  // Enquanto c for menor ou igual a 5 faça:
+  console.log(d);
+}
